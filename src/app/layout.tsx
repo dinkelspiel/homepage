0513858c62bd4ae1729eb849b5b6affb29658899
dinkelspiel <mail@keii.dev>
2024-08-03 14:03:35 +0200

    feat: analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
 
 import { TRPCReactProvider } from "~/trpc/react";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Willem Dinkelspiel",
@@ -16,6 +17,11 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
+      <Script
+        defer
+        data-domain="keii.dev"
+        src="https://plausible.keii.dev/js/script.js"
+      />
       <body className="bg-[#f4f4f4]">
         <TRPCReactProvider>{children}</TRPCReactProvider>
       </body>
